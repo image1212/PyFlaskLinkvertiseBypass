55ee90b89085a05520e3af7f88d915f959d23462
이미지 <86639457+image1212@users.noreply.github.com>
2022-08-02 19:54:36 +0900

    main page upadate

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from flask import Flask,redirect
+from flask import Flask,redirect,request,render_template
 import requests
 
 app = Flask(__name__)
 
 def bypass(url):
-  payload = {"url": url,}
+  payload = {
+    "url": url,
+  }
+
   r = requests.post("https://api.bypass.vip/", data=payload)
   return r.json()
 
-@app.route('/')
-def main():
-    return 'hello world'
+
+@app.route('/',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        id = request.form['URL']
+        link = id
+        bson = bypass(link)
+        blink = bson['destination']
+        return redirect(blink)
+    else:
+        return render_template("index.html")
+
 
 @app.route('/<int:number>/<name>/<int:count>')
 def bp(number,name,count):
@@ -20,5 +32,5 @@ def bp(number,name,count):
     return redirect(blink)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=80)
-    #app.run(host="0.0.0.0", port=443,ssl_context=('certificate', 'key')) HTTPS
+
+    app.run(host="0.0.0.0", port=80)
